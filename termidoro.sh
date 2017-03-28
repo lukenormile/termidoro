535f0c5dcb056b15a96e18171fd5ffe704e0c6db
Luke Normile <luke.normile@outlook.com>
2017-03-28 11:12:13 -0600

    Print number of completed sets after each set

--- termidoro.sh
+++ termidoro.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+WORK_SETS_DONE=0
+REST_SETS_DONE=0
 zenity --warning --text="Start working." 2>/dev/null
+printf "Work sets done: %d\n" "$WORK_SETS_DONE"
 
 while true; do
-	sleep 1500 && zenity --warning --text="take a break" 2>/dev/null;
-	sleep 300 && zenity --warning --text="back to work son" 2>/dev/null;
+	sleep 1500 &&
+	WORK_SETS_DONE++ &&
+	zenity --warning --text="take a break" 2>/dev/null &&
+	printf "Work sets done: %d\n" "$WORK_SETS_DONE"
+
+	CURRENT_REST_SET++ &&
+	sleep 300 &&
+	zenity --warning --text="back to work son" 2>/dev/null;
 done
