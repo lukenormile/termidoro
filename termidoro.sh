e27383740d734ebd506f8b11084136aa0a33f55c
Luke Normile <luke.normile@outlook.com>
2017-03-29 14:57:06 -0600

    Fix syntax error in incrementing WORK_SETS_DONE.

--- termidoro.sh
+++ termidoro.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 WORK_SETS_DONE=0
-REST_SETS_DONE=0
 zenity --warning --text="Start working." 2>/dev/null
 printf "Work sets done: %d\n" "$WORK_SETS_DONE"
 
 while true; do
-	sleep 1500 &&
-	WORK_SETS_DONE++ &&
-	zenity --warning --text="take a break" 2>/dev/null &&
+	sleep 1500;
+	((WORK_SETS_DONE++));
+	zenity --warning --text="take a break" 2>/dev/null;
 	printf "Work sets done: %d\n" "$WORK_SETS_DONE"
 
-	CURRENT_REST_SET++ &&
-	sleep 300 &&
+	sleep 300;
 	zenity --warning --text="back to work son" 2>/dev/null;
 done
